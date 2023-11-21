06017f50a75c23d18281a4a30cee186717c81cf2
Matvey <matveykawork@gmail.com>
2023-11-21 11:07:17 +0300

    model: Video

--- backend/apps/video/models.py
+++ backend/apps/video/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    file = models.FileField(
+        upload_to='video/%m/%d', validators=[
+            FileExtensionValidator(['mp4'])
+        ]
+    )
+
+    def __str__(self):
+        return str(self.title)
+
+
